11b1278173bd694d90255c5dcc20a79eb3ba2452
Roshan Rai <rrai@lb.com>
2022-10-04 18:53:40 +0530

    Initial Change

--- app/src/main/java/com/example/weatherapp/WeatherApplication.kt
+++ app/src/main/java/com/example/weatherapp/WeatherApplication.kt
@@ -1,17 +1,9 @@
 package com.example.weatherapp
 
 import android.app.Application
-import coil.ImageLoader
-import coil.ImageLoaderFactory
-import dagger.hilt.android.HiltAndroidApp
 
-@HiltAndroidApp
-@Suppress("unused")
-class WeatherApplication : Application(), ImageLoaderFactory {
 
-    override fun newImageLoader(): ImageLoader {
-        return ImageLoader.Builder(this)
-            .crossfade(true)
-            .build()
-    }
+class WeatherApplication : Application(){
+
+
 }
