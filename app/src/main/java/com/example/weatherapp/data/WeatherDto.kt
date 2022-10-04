381a7cc117df5f93ca0dae2227df9b942fcbcbc2
Roshan Rai <rrai@lb.com>
2022-10-04 19:49:57 +0530

    Weather Data

---
+++ app/src/main/java/com/example/weatherapp/data/WeatherDto.kt
@@ -0,0 +1,8 @@
+package com.example.weatherapp.data
+
+import com.squareup.moshi.Json
+
+data class WeatherDto(
+    @field:Json(name = "hourly")
+    val weatherData: WeatherDataDto
+)
---
+++ app/src/main/java/com/example/weatherapp/domain/weather/WeatherInfo.kt
@@ -0,0 +1,5 @@
+package com.example.weatherapp.domain.weather
+data class WeatherInfo(
+    val weatherDataPerDay: Map<Int, List<WeatherData>>,
+    val currentWeatherData: WeatherData?
+)
