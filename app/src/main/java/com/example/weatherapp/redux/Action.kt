7f13ac74feda0e5a4fd6e176f325156daf8094dc
Roshan Rai <rrai@lb.com>
2022-10-05 14:04:47 +0530

    Redux setup for MVVM

---
+++ app/src/main/java/com/example/weatherapp/redux/Action.kt
@@ -0,0 +1,8 @@
+package com.example.weatherapp.redux
+/**
+ * While this interface is empty, it is used to clearly define any Action or Intent that was taken
+ * on a given screen.
+ */
+
+interface Action {
+}
---
+++ app/src/main/java/com/example/weatherapp/redux/State.kt
@@ -0,0 +1,7 @@
+package com.example.weatherapp.redux
+
+/**
+ * While this interface is empty, it is used to clearly define any class that is representative
+ * of a View state for a given screen.
+ */
+interface State
